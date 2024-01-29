ee5d2af6fe509a7957baed4bba36beb3fffb0f8f
Michael Dereszynski <mlderes@hotmail.com>
2024-01-29 21:22:49 +0000

    Update data path in budget-cleaner.py

--- budget-cleaner.py
+++ budget-cleaner.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 from pathlib import Path
 import pandas as pd
@@ -6,6 +7,7 @@ EXPENSE_FILE = "ProMIS Expense.xlsx"
 OBLIGATIONS_FILE = "ProMIS Obligations.xlsx"
 DATA = Path("data")
 
+# %% Cell 1
 # Assign the path where the files exists
 # Load the Excel file
 def parse_work_tags_column(file_path):
@@ -28,15 +30,30 @@ def parse_work_tags_column(file_path):
     result_df = pd.concat([df, df_worktags], axis=1)
 
     # Drop the original 'work tags' column if needed
-    result_df.drop(columns=['Worktags'], inplace=True)
+    columns_to_drop = ['Worktags','Accounting Date', 'Operational Transaction', 'Journal',
+       'Revenue Category','AASIS Code', 'Cost Center', 'Designated','Earning', 'Employee Type', 'Fund', 'Job Profile',
+       'Location', 'NACUBO Function', 'Pay Group', 'Pay Rate Type','Personnel Services Restrictions', 'Position', 'Deduction (Workday Owned)', 'Fringe Basis']
+    result_df.drop(columns=columns_to_drop, inplace=True,errors='ignore')
 
     return result_df
 
 expense_df = parse_work_tags_column(EXPENSE_FILE)
 obligation_df = parse_work_tags_column(OBLIGATIONS_FILE)
+# %%
 
 # Create an Excel writer object
-with pd.ExcelWriter(DATA/"ProMIS Expense and Obligations.xlsx") as writer:
+with pd.ExcelWriter(DATA/"results.xlsx") as writer:
     # Write the DataFrame to the Excel file in a new sheet
     expense_df.to_excel(writer, sheet_name='Expense', index=False)
     obligation_df.to_excel(writer, sheet_name='Obligations', index=False)
+    
+    # # Create a sheet with a pivot table of the expense data
+    # expense_pivot = pd.pivot_table(expense_df, index=["Ledger Account","Spend Category", "Employee"], values='Transaction Amount', aggfunc='sum')
+    # obligation_pivot = pd.pivot_table(obligation_df, index=["Ledger Account","Spend Category", "Employee"], values='Transaction Amount', aggfunc='sum')
+    
+    # # Write the pivot table to the Excel file
+    # expense_pivot.to_excel(writer, sheet_name='Expense Pivot')
+    # obligation_pivot.to_excel(writer, sheet_name='Obligations Pivot')
+    
+
+# %%
