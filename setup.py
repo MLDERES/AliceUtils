c9deaf58a5b95c5a169f55839a3418c8b4534415
Michael Dereszynski <mlderes@hotmail.com>
2024-04-10 00:36:17 -0500

    Rename file

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='cleaner',
+    name='budget_fix',
     version='0.1.0',
-    py_modules=['cleaner'],
+    py_modules=['budget_fix'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'cleaner = cleaner:process_files',
+            'budget_fix = budget_fix:process_files',
         ],
     },
 )
